068c3a0c3275ef22ee92bba3da949b3cf1423aa3
Brandon Beckett <bb@branbeckett.com>
2020-10-14 22:08:24 +0200

    Add files via upload

--- stock-market/predict.py
+++ stock-market/predict.py
@@ -1,1 +1,10 @@
+import pandas as pd
+from datetime import datetime
 
+sphist = pd.read_csv('sphist.csv')
+
+sphist['Date'] = sphist['Date'].to_datetime
+
+sphist['Date'] = sphist[sphist['Date'] > datetime(year=2015, month=4, day=1)]
+
+sphist['Date'].sort_values()
